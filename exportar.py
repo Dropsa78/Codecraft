47ee08c251f90d16cdb6447300690076be4694e4
Emilio Benjamin Macias Martinez <utm23090635@utma.edu.mx>
2025-08-10 19:30:46 -0600

    Abrir automaticamente excel
    Se abre excel automaticamente al exportar la tabla

--- exportar.py
+++ exportar.py
@@ -1,4 +1,3 @@
-# exportar.py
 import db
 import pandas as pd
 import os
@@ -13,6 +12,7 @@ def exportar_todo(archivo_excel):
     conn = db.conectar()
     tablas = ["empleados", "ventas", "devoluciones", "productos", "pagos"]
 
+    with pd.ExcelWriter(ruta_archivo, engine="openpyxl") as writer:
         for tabla in tablas:
             df = pd.read_sql_query(f"SELECT * FROM {tabla}", conn)
             df.to_excel(writer, sheet_name=tabla, index=False)
